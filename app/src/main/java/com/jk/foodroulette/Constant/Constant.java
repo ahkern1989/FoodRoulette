5535ffda04c88672dd2d8188a107624b434c5d2d
Jacob <shuailiew@hotmail.com>
2016-05-17 18:27:32 +0800

    add save and load image function

---
+++ app/src/main/java/com/jk/foodroulette/Constant/Constant.java
@@ -0,0 +1,7 @@
+package com.jk.foodroulette.Constant;
+
+/**
+ * Created by Jacob on 4/24/2016.
+ */
+public class Constant {
+}
--- app/src/main/java/com/jk/foodroulette/MainActivity.java
+++ app/src/main/java/com/jk/foodroulette/MainActivity.java
@@ -1,13 +1,20 @@
 package com.jk.foodroulette;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,11 +45,41 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == CAM_REQUEST) {
             if (data != null) {
                 Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-                iv_camera.setImageBitmap(thumbnail);
+                //iv_camera.setImageBitmap(thumbnail);
+                saveBitmapFile(thumbnail);
+                loadImageFromStorage();
             }
         }
     }
 
+    public boolean saveBitmapFile(Bitmap image) {
+        try {
+            // Use the compress method on the Bitmap object to write image to
+            // the OutputStream
+            FileOutputStream fos = this.openFileOutput("1.png", Context.MODE_PRIVATE);
+
+            // Writing the bitmap to the output stream
+            image.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.close();
+
+            return true;
+        } catch (Exception e) {
+            Log.e("saveToInternalStorage()", e.getMessage());
+            return false;
+        }
+    }
+
+    private void loadImageFromStorage() {
+
+        try {
+            FileInputStream fis = openFileInput("1.png");
+            Bitmap b = BitmapFactory.decodeStream(fis);
+            iv_camera.setImageBitmap(b);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     class btn_cameraClicker implements Button.OnClickListener {
         @Override
